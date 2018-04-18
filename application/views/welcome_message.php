53c0243a89efa4bdcdd5840eedc472963979945e
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2018-04-18 14:13:16 +0700

    :tada::rocket: Added Template Ela Admin

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,87 +3,60 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
 
-	<style type="text/css">
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('assets/images/favicon.png')?>">
+    <title>Ela - Bootstrap Admin Dashboard Template</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url('assets/css/lib/bootstrap/bootstrap.min.css')?>" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url('assets/css/helper.css')?>" rel="stylesheet">
+    
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
 
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
+    <link href="<?php echo base_url('assets/css/style.css')?>" rel="stylesheet">
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
 
-	#body {
-		margin: 0 15px 0 15px;
-	}
 
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
 
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+
+
 </head>
 <body>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	
+<script src="<?php echo base_url('assets/js/lib/jquery/jquery.min.js')?>"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="<?php echo base_url('assets/js/lib/bootstrap/js/popper.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/lib/bootstrap/js/bootstrap.min.js')?>"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="<?php echo base_url('assets/js/jquery.slimscroll.js')?>"></script>
+    <!--Menu sidebar -->
+    <script src="<?php echo base_url('assets/js/sidebarmenu.js')?>"></script>
+    <!--stickey kit -->
+    <script src="<?php echo base_url('assets/js/lib/sticky-kit-master/dist/sticky-kit.min.js')?>"></script>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
+    <!--  flot-chart js -->
+    <script src="<?php echo base_url('assets/js/lib/flot-chart/excanvas.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/lib/flot-chart/jquery.flot.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/lib/flot-chart/jquery.flot.pie.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/lib/flot-chart/jquery.flot.time.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/lib/flot-chart/jquery.flot.stack.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/lib/flot-chart/jquery.flot.resize.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/lib/flot-chart/jquery.flot.crosshair.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/lib/flot-chart/curvedLines.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/lib/flot-chart/flot-tooltip/jquery.flot.tooltip.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/lib/flot-chart/flot-chart-init.js')?>"></script>
+    <!--Custom JavaScript -->
+    <script src="js/custom.min.js"></script>
 </body>
 </html>
